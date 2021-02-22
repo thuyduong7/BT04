849decad55ba2235ee060f7b8aadc71c37161574
thuyduong7 <78083024+thuyduong7@users.noreply.github.com>
2021-02-22 23:56:24 +0700

    Create Bai12.cpp

---
+++ Bai12.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    bool check[n];
+    for (int i = 2; i <= n; i++) check[i] = true;
+    for (int i = 2; i <= n; i++){
+        if (check[i]){
+            for (int j = i*i; j <= n; j+=i){
+                check[j] = false;
+            }
+        }
+    }
+    for (int i = 2; i <= n; i++){
+        if (check[i]) cout << i << ' ';
+    }
+    return 0;
+}
